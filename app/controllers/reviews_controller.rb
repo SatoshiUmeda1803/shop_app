43dbda0c197fa48540d8cf562baf45bd3e03ab12
SatoshiUmeda1803 <jartashi0911@gmail.com>
2022-09-08 22:12:14 +0900

    add:レビューを作成、一覧で表示できるようにしました

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to product_reviews_path(@review.product), success: 'レビューの投稿に成功しました'
+    else
+      redirect_to product_reviews_path(@review.product), danger: 'レビューの投稿に失敗しました'
+    end
+  end
+
+  def index
+    @product = Product.find(params[:product_id])
+    @reviews = @product.reviews.all
+    @review = Review.new
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:product_id, :name, :comment, :rate, tag_ids: [])
+  end
+
+end
