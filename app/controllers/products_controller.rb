1b0617a9e489ed92a3ef089c04bd8c2f96bc032f
SatoshiUmeda1803 <jartashi0911@gmail.com>
2022-09-08 22:02:19 +0900

    add:レビュー数を取得できるように追記

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,5 +6,6 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
     @manufacturer_products = @product.manufacturer.products.where.not(id: params[:id])
+    @number_of_reviews = @product.reviews.count
   end
 end
