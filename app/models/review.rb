525477277ed9f7d80cff1df8e17f537f889f282e
SatoshiUmeda1803 <jartashi0911@gmail.com>
2022-09-08 22:13:44 +0900

    add:アソシエーション、バリデーションを追記

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ApplicationRecord
+  belongs_to :product
+  has_many :review_tag_relations, dependent: :destroy
+  has_many :tags, through: :review_tag_relations, dependent: :destroy
+
+  validates :name, presence: true
+  validates :comment, presence: true
+  validates :rate, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+end
