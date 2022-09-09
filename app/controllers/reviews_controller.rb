55519270b331c803ab9af088e1b2932bbe077632
SatoshiUmeda1803 <jartashi0911@gmail.com>
2022-09-10 00:33:14 +0900

    add:レビューを作成した際にレビューの総合評価を記録できるようにした レビュー作成時に、製品の評価を、製品についている全てのレビューの平均で取得して更新できるようにした。 レビュー作成時に、製品のレビュー数を取得して更新できるようにした ’

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,14 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
+    scores =[]
+    scores.push( @review.price_rate ,@review.design_rate , @review.quality_rate)
+    @review.rate = scores.sum.fdiv(scores.length).round(1)
     if @review.save
+      product = Product.find(params[:product_id])
+      product.rate = product.avg_score
+      product.review_count = product.count_reviews
+      product.save
       redirect_to product_reviews_path(@review.product), success: 'レビューの投稿に成功しました'
     else
       redirect_to product_reviews_path(@review.product), danger: 'レビューの投稿に失敗しました'
@@ -18,7 +25,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:product_id, :name, :comment, :rate, tag_ids: [])
+    params.require(:review).permit(:product_id, :name, :comment, :price_rate, :design_rate, :quality_rate,  tag_ids: [])
   end
 
 end
